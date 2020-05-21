ba15205e327e4c72fb44ebcdc2e1760d5ed08464
서현욱 <mrseos@naver.com>
2020-05-21 18:36:01 +0900

    Create Main.java
    테트로미노

---
+++ algorithms/src/main/java/baekjoon/삼성기출문제/테트로미노/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.삼성기출문제.테트로미노;
+
+import java.awt.Point;
+
+class shape {
+	Point[] loc;
+	void rotate(int index) {
+		
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+	}
+
+}
