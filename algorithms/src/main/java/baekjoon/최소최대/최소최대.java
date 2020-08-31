fe4e8b619b70da50f68ffff444e1da1a281d9726
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-08-31 20:32:38 +0900

    Create 최소최대.java

---
+++ algorithms/src/main/java/baekjoon/최소최대/최소최대.java
@@ -0,0 +1,28 @@
+package baekjoon.최소최대;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 최소최대 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[0]+" "+arr[arr.length-1]);
+		
+	}
+	
+}
