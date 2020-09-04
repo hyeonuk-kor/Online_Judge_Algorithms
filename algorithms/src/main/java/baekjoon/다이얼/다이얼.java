d041c32811636e43543a17c4c9e49d13fffc9694
서현욱 <mrseos@naver.com>
2020-09-04 12:58:15 +0900

    Update 다이얼.java

--- algorithms/src/main/java/baekjoon/다이얼/다이얼.java
+++ algorithms/src/main/java/baekjoon/다이얼/다이얼.java
@@ -11,9 +11,11 @@ public class 다이얼 {
 		char[] dials = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
 		int time = 0;
 		for(char i: dials) {
+			System.out.print(i+" ");
 			time += (i-'A')/3+3;
-			if(i=='S'||i=='V'||i=='Z')
+			if(i=='S'||i=='V'||i=='Y'||i=='Z')
 				time--;
+			System.out.println(time);
 		}
 		System.out.println(time);
 		
