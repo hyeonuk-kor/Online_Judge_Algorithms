19e6fcbe12697cd3c23c814ad9f245ca0df85663
서현욱 <mrseos@naver.com>
2020-09-04 12:46:57 +0900

    Create 다이얼.java

---
+++ algorithms/src/main/java/baekjoon/다이얼/다이얼.java
@@ -0,0 +1,22 @@
+package baekjoon.다이얼;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 다이얼 {
+
+	public static void main(String[] args) throws IOException {
+		
+		char[] dials = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
+		int time = 0;
+		for(char i: dials) {
+			time += (i-'A')/3+3;
+			if(i=='S'||i=='V'||i=='Z')
+				time--;
+		}
+		System.out.println(time);
+		
+	}
+
+}
