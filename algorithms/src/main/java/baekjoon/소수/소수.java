5eec360284d1bb0bf75f0ecad8ab83acbc9d1778
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-09-06 16:17:59 +0900

    Create 소수.java

---
+++ algorithms/src/main/java/baekjoon/소수/소수.java
@@ -0,0 +1,18 @@
+package baekjoon.소수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 소수 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+	}
+	
+}
