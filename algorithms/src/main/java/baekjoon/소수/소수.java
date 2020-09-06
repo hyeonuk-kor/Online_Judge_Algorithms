cc070fa815f12b750d9dea60d3f9eb7a17e47721
서현욱 <mrseos@naver.com>
2020-09-07 00:04:03 +0900

    Update 소수.java

--- algorithms/src/main/java/baekjoon/소수/소수.java
+++ algorithms/src/main/java/baekjoon/소수/소수.java
@@ -5,14 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 소수 {
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int M = Integer.parseInt(br.readLine());
 		int N = Integer.parseInt(br.readLine());
-		
+
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+
+		for(int i=M; i<=N; i++) {
+			boolean check = true;
+			if(i==1)
+				check = false;
+			else {
+				for(int j=2; j<i; j++) {
+					if(i%j==0) {
+						check = false;
+						break;
+					}
+				}
+			}
+			if(check) {
+				sum += i;
+				if(min > i)
+					min = i;
+			}
+		}
+
+		if(sum==0)
+			System.out.println(-1);
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+
 	}
-	
+
 }
