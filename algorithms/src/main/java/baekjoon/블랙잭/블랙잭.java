08c1198fece23bf15d28fcfa446969eab456d625
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-09-09 01:29:01 +0900

    Create 블랙잭.java

---
+++ algorithms/src/main/java/baekjoon/블랙잭/블랙잭.java
@@ -0,0 +1,37 @@
+package baekjoon.블랙잭;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 블랙잭 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int max = 0;
+		for(int i=0; i<N-2; i++) {
+			for(int j=i+1; j<N-1; j++) {
+				for(int k=j+1; k<N; k++) {
+					int sum = arr[i]+arr[j]+arr[k];
+					if(sum > max && sum <= M) {
+						max = sum;
+					}
+				}
+			}
+		}
+		
+		System.out.println(max);
+	}
+	
+}
