fc12be917804cf0746c78b013f22cac0ec9175f4
서현욱 <mrseos@naver.com>
2020-09-10 19:31:36 +0900

    Update 수정렬하기3.java

--- algorithms/src/main/java/baekjoon/수정렬하기3/수정렬하기3.java
+++ algorithms/src/main/java/baekjoon/수정렬하기3/수정렬하기3.java
@@ -1,7 +1,9 @@
 package baekjoon.수정렬하기3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class 수정렬하기3 {
 	public static void main(String[] args) throws Exception {
@@ -9,20 +11,24 @@ public class 수정렬하기3 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		int[] check = new int[10000000];
+		int[] check = new int[10001];
 
 		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(br.readLine());
-			check[num]++;
+			check[Integer.parseInt(br.readLine())]++;
 		}
 
-		StringBuilder sb = new StringBuilder();
+//		StringBuilder sb = new StringBuilder();
+		StringBuffer sb = new StringBuffer();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		for (int i = 0; i < check.length; i++) {
-			if(check[i]>0) {
-				for(int j=0; j<check[i]; j++)
-					sb.append(i).append('\n');
-			}
+			while(check[i]-->0)
+//				System.out.println(i);
+				sb.append(i).append('\n');
+//				bw.write(i+"\n");
 		}
 		System.out.println(sb);
+//		bw.write(sb.toString());
+//		bw.flush();
+//		bw.close();
 	}
 }
