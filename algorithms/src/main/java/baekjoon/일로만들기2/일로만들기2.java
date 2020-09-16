048eeb72f4f5c78e2e7cc22ebb38d72cec71b480
서현욱 <mrseos@naver.com>
2020-09-16 22:12:38 +0900

    Update 일로만들기2.java

--- algorithms/src/main/java/baekjoon/일로만들기2/일로만들기2.java
+++ algorithms/src/main/java/baekjoon/일로만들기2/일로만들기2.java
@@ -13,45 +13,24 @@ public class 일로만들기2 {
 		
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N+1];
-		int result = solve(N, arr);
-		sb.append(result).append('\n');
 		
-
-		//System.out.println(sb);
-	}
-
-
-
-	private static int solve(int n, int[] arr) {
-		if(arr[n]!=0)
-			return arr[n];
-		
-		switch(n) {
-		case 1:
-			arr[n] = 0;
-			break;
-		case 2:
-		case 3:
-			arr[n] = 1;
-			break;
-		default:
-			if(n%3==0) {
-				int check = solve(n/3, arr);
-				arr[n] = check + 1;
+		for(int i=2; i<N+1; i++) {
+			arr[i] = arr[i-1] + 1;
+			if(i%3==0) {
+				arr[i] = Math.min(arr[i], arr[i/3]+1);
 			}
-			if(n%2==0) {
-				int check = solve(n/2, arr);
-				if(arr[n]==0 || arr[n] > check + 1)
-					arr[n] = check + 1;
+			if(i%2==0) {
+				arr[i] = Math.min(arr[i], arr[i/2]+1);
 			}
-			
-			int check = solve(n-1, arr);
-			if(arr[n]==0 || arr[n] > check +1)
-				arr[n] = check + 1;
-			break;
+		}
+
+		int solution = N;
+		for(int i=N-1; i>=0; i--) {
+			if(arr[i]==solution) {
+			}
 		}
 		
-		return arr[n];
+		System.out.println(arr[N]);
 	}
-	
+
 }
