19fb2404ee8cdeab6cb292590723b89e281a42f0
서현욱 <mrseos@naver.com>
2020-09-18 17:05:04 +0900

    Update N과M.java

--- algorithms/src/main/java/baekjoon/N과M/N과M.java
+++ algorithms/src/main/java/baekjoon/N과M/N과M.java
@@ -1,9 +1,53 @@
 package baekjoon.N과M;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class N과M {
 	
-	public static void main(String[] args) {
-			 
+	static int N, M;
+	static List<Integer> permArr;
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken()); 
+		M = Integer.parseInt(st.nextToken());
+		
+		sb = new StringBuilder();
+		permArr = new ArrayList<>();
+		for(int i=1; i<=N; i++) {
+			permArr.add(i);
+			perm(i);
+			permArr.remove(new Integer(i));
+		}
+		
+		bw.write(sb.toString());
+		bw.close();
 	}
-	
+	private static void perm(int number) {
+		if(permArr.size()==M) {
+			for (Integer i : permArr) {
+				sb.append(i).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			if(permArr.contains(i))
+				continue;
+			permArr.add(i);
+			perm(i);
+			permArr.remove(new Integer(i));
+		}
+	}
 }
