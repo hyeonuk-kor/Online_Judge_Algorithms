83a15bdd807c50919f3e5b126ae5175ca8389dd0
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-09-21 01:26:02 +0900

    Update 감시.java

--- algorithms/src/main/java/baekjoon/감시/감시.java
+++ algorithms/src/main/java/baekjoon/감시/감시.java
@@ -2,45 +2,72 @@ package baekjoon.감시;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 감시 {
 
 	static int N,M;
+	static char[][] map;
+	static char[][] orimap;
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		char[][] map = new char[N][M];
-		
-		Queue<int[]> q = new LinkedList<>();
+		map = new char[N][M];
+		orimap = new char[N][M];
+
+		int cameraCount = 0;
+		List<int[]> list = new ArrayList<>();
 		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<M; j++) {
-				map[i][j] = st.nextToken().charAt(0);
-				if(map[i][j]=='1')
-					q.add(new int[] {i,j,map[i][j]-'0',4});
-				else if(map[i][j]=='2')
-					q.add(new int[] {i,j,map[i][j]-'0',2});
-				else if(map[i][j]=='3')
-					q.add(new int[] {i,j,map[i][j]-'0',4});
-				else if(map[i][j]=='4')
-					q.add(new int[] {i,j,map[i][j]-'0',4});
-				else if(map[i][j]=='5')
-					q.add(new int[] {i,j,map[i][j]-'0',1});
-				
+				orimap[i][j] = st.nextToken().charAt(0);
+				if(orimap[i][j]>'0' && orimap[i][j]<'6') {
+					list.add(new int[] {i,j,orimap[i][j]-'0'});
+					cameraCount++;
+				}
 			}
 		}
-//
 		
+		int allCaseCount = (int)Math.pow(4, cameraCount);
+		int min=Integer.MAX_VALUE;
+		
+		for (int i = 0; i < allCaseCount ; i++) {
+			
+			for (int j = 0; j < N; j++) {
+				for(int k = 0; k < M; k++) {
+					map[j][k]=orimap[j][k];
+				}
+			}
+			
+			int rePerm = i;
+			for (int[] is : list) {
+				int dir = rePerm%4;
+				rePerm/=4;
+				simulate(is[0],is[1],is[2],dir+1);
+			}
+			
+			int zeroCount=0;
+			for (int j = 0; j < N; j++) {
+				for(int k = 0; k < M; k++) {
+					if(map[j][k]=='0') {
+						zeroCount++;
+					}
+				}
+			}
+			min = Math.min(min, zeroCount);
+		}
+		
+		System.out.println(min);
+
 		
 	} // end main
 
-	private static void go(int x, int y, char[][] map, int dir, int cameraNumber) {
+	private static void simulate(int y, int x, int cameraNumber,  int dir) {
 		if(cameraNumber==1) {
 			if(dir==4) {
 				for(int i=y; i>=0; i--)
@@ -66,7 +93,6 @@ public class 감시 {
 						map[y][i] = '#';
 					else if(map[y][i]=='6')
 						break;
-			} else {
 			}
 		} else if(cameraNumber==2) {
 			if(dir==2) {
@@ -92,6 +118,7 @@ public class 감시 {
 					else if(map[y][i]=='6')
 						break;
 			} else {
+				return;
 			}
 		} else if(cameraNumber==3) {
 			if(dir==4) {
@@ -138,7 +165,6 @@ public class 감시 {
 						map[y][i] = '#';
 					else if(map[y][i]=='6')
 						break;
-			} else {
 			}
 		} else if(cameraNumber==4) {
 			if(dir==4) {
@@ -205,31 +231,33 @@ public class 감시 {
 						map[y][i] = '#';
 					else if(map[y][i]=='6')
 						break;
-			} else {
-			}
+			} 
 		} else if(cameraNumber==5) {
-			for(int i=y; i>=0; i--) 
-				if(map[i][x]=='0')
-					map[i][x] = '#';
-				else if(map[i][x]=='6')
-					break;
-			for(int i=x; i>=0; i--) 
-				if(map[y][i]=='0')	
-					map[y][i] = '#';
-				else if(map[y][i]=='6')
-					break;
-			for(int i=y; i<N; i++) 
-				if(map[i][x]=='0')
-					map[i][x] = '#';
-				else if(map[i][x]=='6')
-					break;
-			for(int i=x; i<M; i++) 
-				if(map[y][i]=='0')
-					map[y][i] = '#';
-				else if(map[y][i]=='6')
-					break;
+			if(dir==1) {
+				for(int i=y; i>=0; i--) 
+					if(map[i][x]=='0')
+						map[i][x] = '#';
+					else if(map[i][x]=='6')
+						break;
+				for(int i=x; i>=0; i--) 
+					if(map[y][i]=='0')	
+						map[y][i] = '#';
+					else if(map[y][i]=='6')
+						break;
+				for(int i=y; i<N; i++) 
+					if(map[i][x]=='0')
+						map[i][x] = '#';
+					else if(map[i][x]=='6')
+						break;
+				for(int i=x; i<M; i++) 
+					if(map[y][i]=='0')
+						map[y][i] = '#';
+					else if(map[y][i]=='6')
+						break;
+			} else {
+				return;
+			}
 		}
-			
 
 	} // end go
 
