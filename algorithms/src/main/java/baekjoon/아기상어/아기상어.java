881a3821ae90455e5d3451ce8f3c052402f0af1c
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-10-02 23:10:01 +0900

    Create 아기상어.java

---
+++ algorithms/src/main/java/baekjoon/아기상어/아기상어.java
@@ -0,0 +1,14 @@
+package baekjoon.아기상어;
+
+/**
+ * @author https://blog.naver.com/mrseos
+ * boj.kr/
+ */
+public class 아기상어 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
