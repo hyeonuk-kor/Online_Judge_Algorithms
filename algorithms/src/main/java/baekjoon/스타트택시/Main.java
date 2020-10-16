df283b034816315249d6f0862eacf2d6e5dd4a69
서현욱 <mrseos@naver.com>
2020-10-16 22:49:42 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/스타트택시/Main.java
+++ algorithms/src/main/java/baekjoon/스타트택시/Main.java
@@ -2,90 +2,135 @@ package baekjoon.스타트택시;
 import java.util.*;
 import java.io.*;
 public class Main {
-	static final int INF = 987654321;
+	static int N, M, fuel, map[][];
+	static int startFuel, endFuel; 
+	static ArrayList<int[]> passengerInfo;
+	static int si, sj, ei, ej;
+	static int dir[][] = {{1,0},{-1,0},{0,1},{0,-1}};
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int fuel = Integer.parseInt(st.nextToken());
-		int map[][] = new int[N][N];
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j]==1)
-					map[i][j]=INF;
+		input();
+		System.out.println(getResult());
+
+	} //end main
+
+	static int getResult() {
+		for(int i=0; i<M; i++) {
+			if(fuel<=0)
+				return -1;
+			if(!findPassenger())
+				return -1;
+			if(!movePassenger())
+				return -1;
+		}
+		return fuel;
+	} // end getResult
+
+	static boolean movePassenger() {
+		Queue<int[]> q = new ArrayDeque<int[]>();
+		boolean[][] visit = new boolean[N][N];
+		q.add(new int[] {si, sj, 0});
+		visit[si][sj] = true;
+		while(!q.isEmpty()) {
+			int[] info = q.poll();
+			int ci = info[0];
+			int cj = info[1];
+			int distance = info[2];
+			if(startFuel+distance > fuel) return false;
+			if(map[ci][cj]==-1) continue;
+			if(ci==ei && cj==ej) {
+				endFuel = distance;
+				fuel = fuel - startFuel - endFuel + (endFuel*2);
+				si = ci;
+				sj = cj;
+				return true;
+			}
+			for(int i=0; i<4; i++) {
+				int ni = ci+dir[i][0];
+				int nj = cj+dir[i][1];
+				if(ni<0 || nj<0 || ni>=N || nj>=N || map[ni][nj]==-1 || visit[ni][nj]) continue;
+				visit[ni][nj] = true;
+				q.add(new int[] {ni, nj, distance+1});
 			}
 		}
+		return false;
+	} // end movePassenger
 
-		Queue<int[]> taxi = new PriorityQueue<int[]>(new Comparator<int[]>() {
+	static boolean findPassenger() {
+		PriorityQueue<int[]> taxi = new PriorityQueue<int[]>(new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				if(o1[0]>o2[0])
-					return 1;
-				else if(o1[0]==o2[0]) {
-					return o1[1]-o2[1];
-				} else {
-					return -1;
+				for(int i=0; i<3; i++) {
+					int out = compareUsingIndex (i, o1, o2);
+					if (out !=0) return out;
 				}
+				return compareUsingIndex (3, o1, o2);
+			}
+			private int compareUsingIndex(int index, int[] o1, int[] o2){
+				if (o1[index]==o2[index]) return 0;
+				else if (o1[index]>o2[index]) return 1;
+				return -1;
 			}
 		});
+		Queue<int[]> q = new ArrayDeque<int[]>();
+		boolean[][] visit = new boolean[N][N];
+		q.add(new int[] {si, sj, 0});
+		visit[si][sj] = true;
+		while(!q.isEmpty()) {
+			int[] info = q.poll();
+			int ci = info[0];
+			int cj = info[1];
+			int distance = info[2];
+			for(int i=1; i<passengerInfo.size(); i++) 
+				if(passengerInfo.get(i)[0]==ci && passengerInfo.get(i)[1]==cj) 
+					taxi.add(new int[] {distance, ci, cj, i});
+			for(int i=0; i<4; i++) {
+				int ni = ci+dir[i][0];
+				int nj = cj+dir[i][1];
+				if(ni<0 || nj<0 || ni>=N || nj>=N || map[ni][nj]==-1 || visit[ni][nj]) continue;
+				visit[ni][nj] = true;
+				q.add(new int[] {ni, nj, distance+1});
+			}
+		}
+		if(taxi.size()==0) return false;
+		int[] getInfo = taxi.poll();
+		int useFuel = getInfo[0];
+		int passengerIndex = getInfo[3];
+		si = passengerInfo.get(passengerIndex)[0];
+		sj = passengerInfo.get(passengerIndex)[1];
+		ei = passengerInfo.get(passengerIndex)[2];
+		ej = passengerInfo.get(passengerIndex)[3];
+		map[si][sj]=0;
+		passengerInfo.remove(passengerIndex);
+		if(fuel<useFuel)
+			return false;
+		else
+			startFuel= useFuel;
+		return true;
+	} // end findPassenger
+	static void input() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		fuel = Integer.parseInt(st.nextToken());
+		map = new int[N+1][N+1];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++)
+				map[i][j] = -Integer.parseInt(st.nextToken());
+		}
 		st = new StringTokenizer(br.readLine());
-		int ti = Integer.parseInt(st.nextToken());
-		int tj = Integer.parseInt(st.nextToken());
-		taxi.add(new int[] {ti, tj, fuel});
-		
+		si = Integer.parseInt(st.nextToken())-1;
+		sj = Integer.parseInt(st.nextToken())-1;
+		passengerInfo = new ArrayList<>();
+		passengerInfo.add(new int[] {});
 		for(int i=1; i<=M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int pi = Integer.parseInt(st.nextToken());
-			int pj = Integer.parseInt(st.nextToken());
-			map[pi][pj] = i;
-			int di = Integer.parseInt(st.nextToken());
-			int dj = Integer.parseInt(st.nextToken());
-			map[di][dj] = -i;
+			int pi = Integer.parseInt(st.nextToken())-1;
+			int pj = Integer.parseInt(st.nextToken())-1;
+			int di = Integer.parseInt(st.nextToken())-1;
+			int dj = Integer.parseInt(st.nextToken())-1;
+			passengerInfo.add(new int[] {pi,pj,di,dj});
 		}
-		
-		int answer = -1;
-		int dir[][] = {
-				{1,0},
-				{-1,0},
-				{0,1},
-				{0,-1}
-		};
-		while(!taxi.isEmpty()) {
-			int tInfo[] = taxi.poll();
-			int cti = tInfo[0];
-			int ctj = tInfo[1];
-			int ctf = tInfo[2];
-			if(map[cti][ctj]>0) {
-				Queue<int[]> passenger = new ArrayDeque<>();
-				passenger.add(new int[] {cti, ctj, map[cti][ctj]});
-				map[cti][ctj]=0;
-				while(!passenger.isEmpty()) {
-					int[] pInfo = passenger.poll();
-					int mi = pInfo[0];
-					int mj = pInfo[1];
-					int num = pInfo[2];
-					if(map[mi][mj]==-num) break;
-					for (int i = 0; i < dir.length; i++) {
-						int ni = mi + dir[i][0];
-						int nj = mj + dir[i][1];
-						if(ni<0 || nj<0 || ni>=N || nj>=N) {
-							
-						}
-					}
-				}
-			}
-			if(ctf==0) break;
-			for (int i = 0; i < dir.length; i++) {
-				int ni = cti + dir[i][0];
-				int nj = ctj + dir[i][1];
-			}
-			break;
-		}
-		
-		System.out.println(answer);
-		
-	}//end main
+	} // end input
 }//end class
