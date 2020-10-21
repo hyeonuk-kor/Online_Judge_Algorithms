3ce6ffac22f54864d2fd90dc38c711f7c45691a0
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-10-21 11:50:24 +0900

    Create Main.java

---
+++ algorithms/src/main/java/baekjoon/절댓값힙/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.절댓값힙;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if(Math.abs(o1)==Math.abs(o2))
+					return Integer.compare(o1, o2);
+				return Integer.compare(Math.abs(o1), Math.abs(o2));
+			}
+		});
+		int N = sc.nextInt();
+		for(int i=0; i<N; i++) {
+			int number = sc.nextInt();
+			if(number==0) {
+				if(pq.size()==0) {
+					sb.append(0).append('\n');
+				} else {
+					sb.append(pq.poll()).append('\n');
+				}
+			} else {
+				pq.add(number);
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
