a5ef38c9ab42a145ec1401accdfb3cc96cf3c4a0
서현욱 <mrseos@naver.com>
2020-10-21 23:26:35 +0900

    거의최단경로작업중
    작업중

--- algorithms/src/main/java/baekjoon/거의최단경로/Main.java
+++ algorithms/src/main/java/baekjoon/거의최단경로/Main.java
@@ -1,61 +1,48 @@
 package baekjoon.거의최단경로;
 import java.util.*;
 import java.io.*;
-
 class Node implements Comparable<Node> {
-	
-	private int index;
-	private int distance;
-	
+	private int index, distance;
 	public Node(int index, int distance) {
-		this.index = index;
-		this.distance = distance;
+		this.setIndex(index);
+		this.setDistance(distance);
 	}
-
-	public int getIndex() {
-		return index;
+	@Override
+	public int compareTo(Node o) {
+		return Integer.compare(this.distance, o.distance);
 	}
-	
-	
-	public void setDistance(int distance) {
-		this.distance = distance;
-	}
-
 	public int getDistance() {
 		return distance;
 	}
-
-	@Override
-	public int compareTo(Node o) {
-//		if(this.distance < o.distance)
-//			return -1;
-//		return 1;
-		return this.distance - o.distance;
+	public void setDistance(int distance) {
+		this.distance = distance;
 	}
-	
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
 }
 
 public class Main {
 	static final int INF = (int)1e9;
-	static int n, m, start, end;
-	static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-	static int[] d = new int[10001];
+	static int n, m, start, end, dist[];
+	static List<Node> adj[];
 	
 	public static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		pq.offer(new Node(start, 0));
-		d[start]=0;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
 		while(!pq.isEmpty()) {
 			Node node = pq.poll();
-			int now = node.getIndex();
-			int dist = node.getDistance();
-			
-			if(d[now]<dist) continue;
-			for (int i = 0; i < graph.get(now).size(); i++) {
-				int cost = d[now] + graph.get(now).get(i).getDistance();
-				if(cost < d[graph.get(now).get(i).getIndex()]) {
-					d[graph.get(now).get(i).getIndex()] = cost;
-					pq.add(new Node(graph.get(now).get(i).getIndex(), cost));
+			int curIndex = node.getIndex();
+			int curWeight = node.getDistance();
+			for(int i=0; i<adj[n].size(); i++) {
+				int nextIndex = adj[curIndex].get(i).getIndex();
+				int nextWeight = adj[curIndex].get(i).getDistance();
+				if(curWeight + nextWeight < dist[nextIndex]) {
+					dist[nextIndex] = curWeight + nextWeight;
+					pq.add(new Node(nextIndex, curWeight + nextWeight));
 				}
 			}
 		}
@@ -73,9 +60,10 @@ public class Main {
 			st = new StringTokenizer(br.readLine()," ");
 			start = Integer.parseInt(st.nextToken())+1;
 			end = Integer.parseInt(st.nextToken())+1;
-			//그래프 초기화
+			dist = new int[n+1];
+			adj = new ArrayList[n+1];
 			for (int i = 0; i <= n; i++) {
-				graph.add(new ArrayList<>());
+				adj[i] = new ArrayList<>();
 			}
 			
 			for (int i = 0; i < m; i++) {
@@ -83,45 +71,29 @@ public class Main {
 				int u = Integer.parseInt(st.nextToken())+1;
 				int v = Integer.parseInt(st.nextToken())+1;
 				int p = Integer.parseInt(st.nextToken())+1;
-				
-				graph.get(u).add(new Node(v,p));
+				adj[u].add(new Node(v,p));
 			}
 			
-			Arrays.fill(d, INF);
-			dijkstra(start);
-			remove(end);
 			for(int i=1; i<=n; i++) {
 				if(i==start) {
-					d[i] = 0;
+					dist[i] = 0;
 				} else {
-					d[i] = INF;
+					dist[i] = INF;
 				}
 			}
+			
 			dijkstra(start);
-			for (int i = 1; i <= n; i++) {
-	            // 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
-	            if (d[i] == INF) {
-	                System.out.println("-1");
-	            }
-	            // 도달할 수 있는 경우 거리를 출력
-	            else {
-	                System.out.println(d[end]);
-	            }
-	        }
+			remove(end);
+			dijkstra(start);
 		}
 		
 	}
 
 	public static void remove(int end) {
-		Queue<Node> q = new ArrayDeque<Node>();
-		q.add(new Node(end, 1));
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		q.add(end);
 		while(!q.isEmpty()) {
-			Node node = q.poll();
-			int now = node.getIndex();
-			int dist = node.getDistance();
-			for (int i = 0; i < graph.get(now).size(); i++) {
-				System.out.println(graph.get(now).get(i).getIndex());
-			}
+			int index = q.peek();
 		}
 	}
 }
