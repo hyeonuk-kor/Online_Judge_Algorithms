268e857ebfd6c875b25b93bf5830f43b27f1e97a
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-10-21 13:16:02 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/숫자의신/Main.java
+++ algorithms/src/main/java/baekjoon/숫자의신/Main.java
@@ -1,33 +1,34 @@
 package baekjoon.숫자의신;
 import java.util.*;
 public class Main {
-	static int K, N, number[];
-	static int[] arr;
-	static boolean[] check;
+	static int K, N;
+	static String number[];
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		K = sc.nextInt();
 		N = sc.nextInt();
-		number = new int[K];
-		arr = new int[N];
-		check = new boolean[N];
-		for(int i=0; i<K; i++) 
-			number[i] = sc.nextInt();
-		Arrays.parallelSort(number);
-		dfs(0);
-	}
-	static void dfs(int idx) {
-		if(idx==N) {
-			System.out.println(Arrays.toString(arr));
-			return;
+		number = new String[N];
+		
+		int max = Integer.MIN_VALUE;
+		for(int i=0; i<K; i++) {
+			number[i] = sc.next();
+			if(Integer.parseInt(number[i])>max) 
+				max = Integer.parseInt(number[i]);
 		}
 		
-		for(int i=0; i<K; i++) {
-			if(check[i]) continue;
-			check[i] = true;
-			arr[i] = number[i];
-			dfs(idx+1);
-			check[i] = false;
-		}
+		for(int i=K; i<N; i++)
+			number[i] = max+"";
+		
+		Arrays.sort(number, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return -o1.concat(o2).compareTo(o2.concat(o1));
+			}
+		});
+		
+		for(String i: number)
+			System.out.print(i);
+		System.out.println();
+		
 	}
 }
