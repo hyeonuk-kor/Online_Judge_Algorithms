1fcd10f3d31517c603c4fec6d8b8500e252d7d52
서현욱 <mrseos@naver.com>
2020-11-05 13:50:11 +0900

    Create Main.java

---
+++ algorithms/src/main/java/baekjoon/마이크로소프트로고/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.마이크로소프트로고;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.append("       _.-;;-._\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-;;-._|\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-''-._|");
+		bw.flush();
+	}
+}
