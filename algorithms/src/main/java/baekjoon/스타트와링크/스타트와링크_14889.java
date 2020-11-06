0b807d55c06f64300245bb5c39f2d44dc1eb5aed
서현욱 <mrseos@naver.com>
2020-11-06 22:15:11 +0900

    Update 스타트와링크_14889.java

--- algorithms/src/main/java/baekjoon/스타트와링크/스타트와링크_14889.java
+++ algorithms/src/main/java/baekjoon/스타트와링크/스타트와링크_14889.java
@@ -1,24 +1,15 @@
 package baekjoon.스타트와링크;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class 스타트와링크_14889 {
-
 	static int N, min=Integer.MAX_VALUE;
+	static int startTeam[], linkTeam[];
 	static int[][] number;
-	static List<Integer> startTeam, linkTeam;
+	static boolean[] check;
 	public static void main(String[] args) throws Exception {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		
 		number = new int[N][N];
-		
 		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -26,34 +17,39 @@ public class 스타트와링크_14889 {
 				number[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		startTeam = new ArrayList<>();
-		linkTeam = new ArrayList<>();
-		getTeam();
-		
+		startTeam = new int[N/2];
+		linkTeam = new int[N/2];
+		check = new boolean[N];
+		getTeam(0, 0);
 		System.out.println(min);
 	}
-	
-	private static void getTeam() {
-		if(startTeam.size()==N/2) {
-			System.out.println(startTeam);
-			System.out.println(linkTeam);
-			System.out.println();
-			int start = 0;
-			int link = 0;
+	private static void getTeam(int start, int idx) {
+		if(idx==N/2) {
+			int linkTeamIdx = 0;
+			for(int i=0; i<check.length; i++) {
+				if(!check[i])
+					linkTeam[linkTeamIdx++] = i;
+			}
+			int startTeamSum = 0, linkTeamSum = 0;
+			for(int i=0; i<N/2-1; i++) {
+				for(int j=i+1; j<N/2; j++) {
+					// startTeam 계산
+					int si = startTeam[i], sj = startTeam[j];
+					startTeamSum = startTeamSum + number[si][sj] + number[sj][si]; 
+					// linkTeam 계산
+					int li = linkTeam[i],  lj = linkTeam[j];
+					linkTeamSum = linkTeamSum + number[li][lj] + number[lj][li]; 
+				}
+			}
+			min = Math.min(Math.abs(startTeamSum-linkTeamSum), min);
 			return;
 		}
-		
-		for(Integer i=0; i<N; i++) {
-			if(startTeam.contains(i))
-				continue;
-			startTeam.add(i);
-			linkTeam.add(N-1-i);
-			getTeam();
-			startTeam.remove(startTeam.size()-1);
-			linkTeam.remove(linkTeam.size()-1);
+		for(int i=start; i<N; i++) {
+			if(check[i]) continue;
+			check[i] = true;
+			startTeam[idx] = i;
+			getTeam(i+1, idx+1);
+			check[i] = false;
 		}
-		
 	}
-	
 }
