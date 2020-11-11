eecc4fb0c2b9b8f3bc77bbb510f49d4f5bb2d3d1
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-11-12 01:00:13 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/에디터/Main.java
+++ algorithms/src/main/java/baekjoon/에디터/Main.java
@@ -7,40 +7,34 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		LinkedList<Character> d = new LinkedList<>();
-		
-		String input = br.readLine();
-		int c = input.length();
+		List<Character> d = new LinkedList<>();
 		ListIterator<Character> iter = d.listIterator();
-		for(int i=0; i<c; i++) {
+		String input = br.readLine();
+		for(int i=0; i<input.length(); i++) 
 			iter.add((Character)input.charAt(i));
-		}
 		
 		int M = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		int r = c-1;
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			String cmd = st.nextToken();
-			if(cmd.equals("P")) { //문자를 커서 왼쪽에 추가
-				char addChar = st.nextToken().charAt(0);
-				iter.add(addChar);
-				c++;
-				r++;
-			} else if(cmd.equals("D")) { //커서 오른쪽으로 이동, 맨뒤면 무시
-				if(r+1==c) continue;
-				iter.next();
-				c++;
-			} else if(cmd.equals("L")) { //커서 왼쪽으로 이동, 맨 앞 무시
-				if(c==0) continue;
-				iter.previous();
-				c--;
-			} else { //B // 커서 왼쪽 문자를 삭제 맨 앞 무시
-				if(c==0) continue;
-				r--;
-				c--;
-				iter.previous();
-				iter.remove();
+			switch (cmd) {
+				case "L":
+					if(!iter.hasPrevious()) continue;
+					iter.previous();
+					break;
+				case "D":
+					if(!iter.hasNext()) continue;
+					iter.next();
+					break;
+				case "B":
+					if(!iter.hasPrevious()) continue;
+					iter.previous();
+					iter.remove();
+					break;
+				case "P":
+					iter.add(st.nextToken().charAt(0));
+					break;
 			}
 		}
 
