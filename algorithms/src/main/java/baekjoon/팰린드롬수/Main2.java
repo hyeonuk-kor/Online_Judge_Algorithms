47d44028fa2c01539567946b4048c38dc689bd8c
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-11-12 00:35:02 +0900

    Create Main2.java

---
+++ algorithms/src/main/java/baekjoon/팰린드롬수/Main2.java
@@ -0,0 +1,41 @@
+package baekjoon.팰린드롬수;
+import java.util.*;
+import java.io.*;
+public class Main2 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String input = br.readLine();
+		
+		while(!input.equals("0")) {
+			List<Character> list = new LinkedList<>();
+			ListIterator<Character> right = list.listIterator();
+			int len = input.length();
+
+			for(int i=0; i<len; i++) {
+				right.add(input.charAt(i));
+			}
+
+			ListIterator<Character> left = list.listIterator();
+
+			boolean check = true;
+			while(len-->0) {
+				if(left.next()==right.previous()) {
+					continue;
+				} else {
+					check = false;
+					break;
+				}
+			}
+			if(check) {
+				bw.append("yes\n");
+			} else {
+				bw.append("no\n");
+			}
+			input = br.readLine();
+		}
+
+		bw.flush();
+	}
+}
