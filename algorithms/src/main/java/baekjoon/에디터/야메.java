cce8051ade4f8fbb58a4f68314e91730050e7577
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-11-12 01:35:43 +0900

    Update 야메.java

--- algorithms/src/main/java/baekjoon/에디터/야메.java
+++ algorithms/src/main/java/baekjoon/에디터/야메.java
@@ -1,8 +1,6 @@
 package baekjoon.에디터;
 import java.util.*;
 import java.io.*;
-import java.util.*;
-import java.io.*;
 public class 야메 {
 
 	public static void main(String[] args) throws Exception {
