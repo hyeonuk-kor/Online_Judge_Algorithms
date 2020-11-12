3fde70534eed8cc1ee83d11bd6c604715bc3fa44
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-11-12 19:52:22 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/큰수BIG/Main.java
+++ algorithms/src/main/java/baekjoon/큰수BIG/Main.java
@@ -1,13 +1,17 @@
 package baekjoon.큰수BIG;
 import java.io.*;
-import java.math.*;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BigInteger MOD = new BigInteger("20000303");
-		BigInteger N = new BigInteger(br.readLine());
-		BigInteger temp = new BigInteger(Math.pow(10, 1000000)+"");
-		System.out.println(temp.mod(MOD));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String input = br.readLine();
+		int answer = 0;
+		for (char c : input.toCharArray()) {
+			answer = (answer*10+c-'0')%20000303;
+		}
+		bw.append(answer+"\n");
+		bw.flush();
 	}
 	
 }
