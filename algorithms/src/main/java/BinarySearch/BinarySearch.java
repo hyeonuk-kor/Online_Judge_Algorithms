34d9579d0c3b6b68aae3f3051bf389371fae02d3
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-11-15 20:23:15 +0900

    Update BinarySearch.java

--- algorithms/src/main/java/BinarySearch/BinarySearch.java
+++ algorithms/src/main/java/BinarySearch/BinarySearch.java
@@ -9,8 +9,10 @@ public class BinarySearch {
 		int target = 12;
 		int findIndex =	binarySearch(array, target, 0, array.length-1);
 		int findIndex2 = Arrays.binarySearch(array, target);
+		int findIndex3 =	binarySearch2(array, target, 0, array.length-1);
 		System.out.println(findIndex);
 		System.out.println(findIndex2);
+		System.out.println(findIndex3);
 	}
 
 	static int binarySearch(int[] array, int target, int start, int end) {
@@ -24,6 +26,19 @@ public class BinarySearch {
 			return binarySearch(array, target, start, mid-1);
 		else
 			return binarySearch(array, target, mid+1, end);
+	}
+	
+	static int binarySearch2(int[] array, int target, int start, int end) {
+		while(start<=end) {
+			int mid = (start+end)/2;
+			if(array[mid]==target)
+				return mid;
+			else if(array[mid]>target)
+				end = mid - 1;
+			else
+				start = mid + 1;
+		}
+		return -1;
 	}
 
 }
