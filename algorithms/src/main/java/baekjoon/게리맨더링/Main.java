efa0545f6e31c6430100cbe2b4fa0a27953ed04e
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-12-01 00:32:13 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/게리맨더링/Main.java
+++ algorithms/src/main/java/baekjoon/게리맨더링/Main.java
@@ -1,10 +1,32 @@
 package baekjoon.게리맨더링;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int[] v, p;
+	static ArrayList<ArrayList<Integer>> graph;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(st.nextToken());
+		v = new int[N];
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i=0; i<N; i++)  {
+			v[i] = Integer.parseInt(st.nextToken());
+			p[i] = i;
+		}
+		
+		graph = new ArrayList<>();
+		for(int i=0; i<N; i++) {
+			ArrayList<Integer> d = new ArrayList<>();
+			st = new StringTokenizer(br.readLine()," ");
+			int c = Integer.parseInt(st.nextToken());
+			for(int j=0; j<c; j++)
+				d.add(Integer.parseInt(st.nextToken()));
+			graph.add(d);
+		}
+		
+		
 	}
 
 }
