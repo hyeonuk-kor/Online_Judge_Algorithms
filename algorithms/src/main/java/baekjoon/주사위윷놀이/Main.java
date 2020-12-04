65405febf6cbb6bdf373032e695818d78514343c
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-12-04 14:22:34 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/주사위윷놀이/Main.java
+++ algorithms/src/main/java/baekjoon/주사위윷놀이/Main.java
@@ -2,9 +2,9 @@ package baekjoon.주사위윷놀이;
 import java.io.*;
 import java.util.*;
 class Node {
-	Node next, pNext;
+	Node next, specialPoint;
 	int number;
-	boolean check;
+	boolean isCombine;
 	public Node(int number) {
 		this.number = number;
 	}
@@ -25,70 +25,77 @@ class Node {
 }
 public class Main {
 	static int dice[] = new int[10], order[] = new int[10], answer;
-	static Node[] horse = new Node[4];
+	static Node[] coin = new Node[4];
 	static Node start, end;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		boardInit();
+		mapInit();
 		for(int i=0; i<10; i++) 
 			dice[i] = Integer.parseInt(st.nextToken());
-		setHorse(0);
+		setYut(0);
 		System.out.println(answer);
 	}
-	static void setHorse(int index) {
+	static void setYut(int index) {
 		if(index==10) {
 			answer = Math.max(answer, simulation(0));
 			return;
 		} 
 		for(int i=0; i<4; i++) {
 			order[index] = i;
-			setHorse(index+1);
+			setYut(index+1);
 		}
 	}
 	static int simulation(int sum) {
 		for(int i=0; i<10; i++) {
-			Node current = horse[order[i]];
-			current.check = false;
+			Node current = coin[order[i]];
+			current.isCombine = false;
 			for(int j=0; j<dice[i]; j++) {
-				if(j==0 && current.pNext!=null) 
-					current = current.pNext;
+				if(j==0 && current.specialPoint!=null) 
+					current = current.specialPoint;
 				else 
 					current = current.next;
 			}
-			horse[order[i]] = current;
-			if(current.check && current!=end) {
+			coin[order[i]] = current;
+			if(current.isCombine && current!=end) {
 				sum = 0;
 				break;
 			} else {
-				current.check = true;
+				current.isCombine = true;
 				sum += current.number;
 			}
 		}
 		for(int i=0; i<4; i++) {
-			horse[i].check = false;
-			horse[i] = start;
+			coin[i].isCombine = false;
+			coin[i] = start;
 		}
 		return sum;
 	}
-	static void boardInit() {
+	static void mapInit() {
 		start = new Node(0);
 		end = new Node(0);
 		end.next = end;
 		Node node = start;
-		for(int i=2; i<=40; i+=2) node = node.addNode(i);
+		for(int i=2; i<=40; i+=2) 
+			node = node.addNode(i);
 		node.next = end;
-		Node centerJunction = new Node(25);
-		Node leftJunction = node.getNode(start, 10);
-		leftJunction.pNext = new Node(13);
-		leftJunction.pNext.addNode(16).addNode(19).next = centerJunction;
-		Node bottomJunction = node.getNode(start, 20);
-		bottomJunction.pNext = new Node(22);
-		bottomJunction.pNext.addNode(24).next = centerJunction;
-		Node rightJunction = node.getNode(start, 30);
-		rightJunction.pNext = new Node(28);
-		rightJunction.pNext.addNode(27).addNode(26).next = centerJunction;
-		centerJunction.addNode(30).addNode(35).next = node.getNode(start, 40);
-		Arrays.fill(horse, start);
+		
+		Node center = new Node(25);
+		
+		Node leftSP = node.getNode(start, 10);
+		leftSP.specialPoint = new Node(13);
+		leftSP.specialPoint.addNode(16).addNode(19).next = center;
+		
+		Node bottomSP = node.getNode(start, 20);
+		bottomSP.specialPoint = new Node(22);
+		bottomSP.specialPoint.addNode(24).next = center;
+		
+		Node rightSP = node.getNode(start, 30);
+		rightSP.specialPoint = new Node(28);
+		rightSP.specialPoint.addNode(27).addNode(26).next = center;
+		
+		center.addNode(30).addNode(35).next = node.getNode(start, 40);
+		
+		Arrays.fill(coin, start);
 	}
 }
