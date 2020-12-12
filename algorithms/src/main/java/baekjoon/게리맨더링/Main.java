c32f008e8c2d328356f54fe7f1b3e703acbebf94
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2020-12-12 21:26:47 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/게리맨더링/Main.java
+++ algorithms/src/main/java/baekjoon/게리맨더링/Main.java
@@ -2,31 +2,58 @@ package baekjoon.게리맨더링;
 import java.util.*;
 import java.io.*;
 public class Main {
-	static int[] v, p;
-	static ArrayList<ArrayList<Integer>> graph;
+	static int N, people[], answer = Integer.MAX_VALUE;
+	static boolean graph[][], distinct[], visit[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		
-		int N = Integer.parseInt(st.nextToken());
-		v = new int[N];
+		N = Integer.parseInt(br.readLine());
+		people = new int[N+1];
 		st = new StringTokenizer(br.readLine()," ");
-		for(int i=0; i<N; i++)  {
-			v[i] = Integer.parseInt(st.nextToken());
-			p[i] = i;
-		}
-		
-		graph = new ArrayList<>();
-		for(int i=0; i<N; i++) {
-			ArrayList<Integer> d = new ArrayList<>();
+		for(int i=1; i<=N; i++)  
+			people[i] = Integer.parseInt(st.nextToken());
+		graph = new boolean[N+1][N+1];
+		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine()," ");
-			int c = Integer.parseInt(st.nextToken());
-			for(int j=0; j<c; j++)
-				d.add(Integer.parseInt(st.nextToken()));
-			graph.add(d);
+			int cnt = Integer.parseInt(st.nextToken());
+			for(int j=0; j<cnt; j++) {
+				int next = Integer.parseInt(st.nextToken());
+				graph[i][next] = true;
+			}
 		}
-		
-		
+		distinct = new boolean[N+1];
+		divideDistinct(1);
+		System.out.println(answer==Integer.MAX_VALUE?-1:answer);
 	}
-
+	static void divideDistinct(int index) {
+		if(index==N) {
+			int red = 0, blue = 0;
+			for(int i=1; i<=N; i++) {
+				if(distinct[i]) red+=people[i]; 
+				else blue+=people[i];
+			}
+			visit = new boolean[N+1];
+			int count = 0;
+			for(int i=1; i<=N; i++) {
+				if(!visit[i]) {
+					check(i, distinct[i]);
+					count++;
+				}
+			}
+			if(count==2) 
+				answer = Math.min(answer, Math.abs(red-blue));
+			return;
+		}
+		distinct[index] = true;
+		divideDistinct(index+1);
+		distinct[index] = false;
+		divideDistinct(index+1);
+	}
+	static void check(int n, boolean color) {
+		visit[n] = true;
+		for(int i=1; i<=N; i++) {
+			if(graph[n][i] && !visit[i] && distinct[i]==color)
+				check(i, color);
+		}
+	}
 }
