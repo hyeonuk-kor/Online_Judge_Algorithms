a51e07051b852d706072e0b4922374f33050eb14
서현욱 <mrseos@naver.com>
2021-01-04 16:20:10 +0900

    Update Solution.java

--- algorithms/src/main/java/swexperts/가능한시험점수/Solution.java
+++ algorithms/src/main/java/swexperts/가능한시험점수/Solution.java
@@ -8,24 +8,23 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1; tc<=T; tc++) {
 			int N = Integer.parseInt(br.readLine());
-			int number[] = new int[N];
-			int result[] = new int[10001];
+			int score[] = new int[N];
+			boolean result[] = new boolean[10001];
 			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			int max = 0;
-			result[0] = 1;
+			result[0] = true;
 			for(int i=0; i<N; i++) {
-				number[i] = Integer.parseInt(st.nextToken());
-				max += number[i];
+				score[i] = Integer.parseInt(st.nextToken());
+				max += score[i]; // 점수을 입력받을 때 마다, 가능한 최대 점수를 갱신한다.
 				for(int j=max; j>=0; j--) {
-					if(result[j]>0) {
-						result[j+number[i]]++;
+					if(result[j]) { //가능 한 점수라면
+						result[j+score[i]] = true; //새롭게 입력받았던 점수를 더해가며 체크해준다.
 					}
 				}
-				result[number[i]]++;
 			}
 			int answer = 0;
 			for(int i=0; i<10001; i++) {
-				if(result[i]>0)
+				if(result[i])
 					answer++;
 			}
 			bw.append("#"+tc+" "+answer+'\n');
