8c25ce1789164425ab2a53a80c5fb215f4e89d4f
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-01-13 00:05:51 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/정수삼각형/Main.java
+++ algorithms/src/main/java/baekjoon/정수삼각형/Main.java
@@ -4,21 +4,16 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		int N = Integer.parseInt(br.readLine());
-		int table[][] = new int[N][];
+		int table[][] = new int[N][N];
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			table[i] = new int[i+1];
-			for(int j=0; j<i+1; j++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			for(int j=0; j<i+1; j++)
 				table[i][j] = Integer.parseInt(st.nextToken());
-			}
 		}
-		for(int i=N-1; i>=1; i--) {
-			for(int j=0; j<i; j++) {
-				table[i-1][j] = Math.max(table[i][j], table[i][j+1]) + table[i-1][j];
-			}
-		}
+		for(int i=N-1; i>=1; i--)
+			for(int j=0; j<i; j++)
+				table[i-1][j] += Math.max(table[i][j], table[i][j+1]);
 		System.out.println(table[0][0]);
 	}
 }
