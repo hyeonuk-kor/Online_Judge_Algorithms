1a49e2fe2ffbb98bedfdd23e1524509b49e8decb
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-01-15 20:03:47 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/오큰수/Main.java
+++ algorithms/src/main/java/baekjoon/오큰수/Main.java
@@ -7,7 +7,6 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		int result[] = new int[N];
-		Arrays.fill(result, -1);
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		Stack<int[]> s = new Stack<>();
 		for(int i=0; i<N; i++) {
@@ -16,8 +15,10 @@ public class Main {
 				result[s.pop()[1]] = number;
 			s.push(new int[] {number, i});
 		}
-		for(int i=0; i<N; i++) 
-			bw.append(result[i]+" ");
+		for(int r: result) {
+			if(r==0) r=-1;
+			bw.append(r+"\n");
+		}
 		bw.flush();
 	}
 }
