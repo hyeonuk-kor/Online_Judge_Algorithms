6b02dcf1f005b7bae001f10bcc80682d917d9216
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-01-20 21:00:05 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/검문/Main.java
+++ algorithms/src/main/java/baekjoon/검문/Main.java
@@ -1,19 +1,19 @@
 package baekjoon.검문;
 import java.io.*;
 import java.math.*;
-import java.util.*;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		BigInteger number[] = new BigInteger[N];
-		for(int i=0; i<N; i++) 
+		number[0] = new BigInteger(br.readLine());
+		number[1] = new BigInteger(br.readLine());
+		BigInteger diffGCD = number[1].subtract(number[0]).abs();
+		for(int i=2; i<N; i++) {
 			number[i] = new BigInteger(br.readLine());
-		Arrays.sort(number);
-		BigInteger diffGCD = number[1].subtract(number[0]);
-		for(int i=2; i<N; i++) 
-			diffGCD = diffGCD.gcd(number[i].subtract(number[i-1]));
+			diffGCD = diffGCD.gcd(number[i].subtract(number[i-1]).abs());
+		}
 		int gcd = diffGCD.intValue();
 		for(int i=2; i<=gcd; i++) 
 			if(gcd%i==0)
