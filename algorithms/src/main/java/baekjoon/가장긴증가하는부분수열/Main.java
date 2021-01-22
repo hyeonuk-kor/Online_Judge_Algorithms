d0be4affda4c1a6b53f3163ed4840c7279396232
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-01-22 14:24:01 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/가장긴증가하는부분수열/Main.java
+++ algorithms/src/main/java/baekjoon/가장긴증가하는부분수열/Main.java
@@ -2,27 +2,30 @@ package baekjoon.가장긴증가하는부분수열;
 import java.io.*;
 import java.util.*;
 public class Main {
+	static int memo[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		ArrayList<Integer> arr = new ArrayList<>();
+		memo = new int[N];
+		Arrays.fill(memo, -1);
 		for(int i=0; i<N; i++) 
 			arr.add(Integer.parseInt(st.nextToken()));
-		System.out.println(getResult(arr));
+		int answer = 1;
+		for(int i=0; i<N; i++)
+			answer = Math.max(answer,getResult(arr, i));
+		System.out.println(answer);
+		System.out.println(Arrays.toString(memo));
 	}
-	static int getResult(ArrayList<Integer> arr) {
-		if(arr.size()==0)
-			return 0;
-		int result = 0;
-		for(int i=0; i<arr.size(); i++) {
-			ArrayList<Integer> temp = new ArrayList<>();
-			for(int j=i+1; j<arr.size(); j++) {
-				if(arr.get(i)<arr.get(j))
-					temp.add(arr.get(j));
-			}
-			result = Math.max(result, 1+getResult(temp));
+	static int getResult(ArrayList<Integer> arr, int start) {
+		if(memo[start]!=-1)
+			return memo[start];
+		int result = 1;
+		for(int next=start+1; next<arr.size(); next++) {
+			if(arr.get(start)<arr.get(next))
+				result = Math.max(result, 1+getResult(arr, next));
 		}
-		return result;
+		return memo[start]=result;
 	}
 }
