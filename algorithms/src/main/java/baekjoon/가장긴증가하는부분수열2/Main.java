608ad1d6b3273a03ee22fdb45a9fc7b3beaa1e3c
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-01-22 19:46:44 +0900

    Create Main.java

---
+++ algorithms/src/main/java/baekjoon/가장긴증가하는부분수열2/Main.java
@@ -0,0 +1,36 @@
+package baekjoon.가장긴증가하는부분수열2;
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		ArrayList<Integer> arr = new ArrayList<>();
+		ArrayList<Integer> lis = new ArrayList<>();
+		for(int i=0; i<N; i++) 
+			arr.add(Integer.parseInt(st.nextToken()));
+		lis.add(arr.get(0));
+		int idx = 1;
+		int temp = 0;
+		for(int i=1; i<N; i++) {
+			if(lis.get(idx-1)<arr.get(i)) {
+				lis.add(idx++, arr.get(i));
+			} else if(lis.get(0)>arr.get(i)) {
+				lis.set(0, arr.get(i));
+			} else {
+				temp = Collections.binarySearch(lis, arr.get(i));
+				if(temp>=0) {
+					lis.set(temp, arr.get(i));
+				} else {
+					lis.set(-temp-1, arr.get(i));
+				}
+			}
+		}
+		bw.append(idx+"\n");
+		for(int number: lis) 
+			bw.append(number+" ");
+		bw.flush();
+	}
+}
