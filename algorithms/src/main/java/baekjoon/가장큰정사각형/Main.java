d00d34a7fb2ddd4e5e9cd398a6bb029d8db79930
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-02-10 20:08:39 +0900

    Create Main.java

---
+++ algorithms/src/main/java/baekjoon/가장큰정사각형/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.가장큰정사각형;
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int arr[][] = new int[n+1][m+1];
+		for(int i=1; i<=n; i++) {
+			String input = br.readLine();
+			for(int j=1; j<=m; j++) {
+				arr[i][j] = input.charAt(j-1) - '0';
+			}
+		}
+		int max = 0;
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=m; j++) {
+				if(arr[i][j]==1) {
+					arr[i][j] = Math.min(arr[i-1][j-1], arr[i-1][j]);
+					arr[i][j] = Math.min(arr[i][j-1]  , arr[i][j]) + 1;
+					max = Math.max(arr[i][j], max);
+				}
+			}
+		}
+		System.out.println(max*max);
+	}
+}
