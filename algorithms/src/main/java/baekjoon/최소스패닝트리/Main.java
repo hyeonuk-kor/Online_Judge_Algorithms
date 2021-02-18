5784db56dea7b27045b36f295ad85faffcd64a30
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-02-18 17:10:34 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/최소스패닝트리/Main.java
+++ algorithms/src/main/java/baekjoon/최소스패닝트리/Main.java
@@ -18,7 +18,7 @@ class Edge implements Comparable<Edge> {
 		return this.getWeight() - o.getWeight();
 	}
 }
-public class Prim {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
