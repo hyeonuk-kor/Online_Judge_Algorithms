360f3c27a2048fc331b732e0144036bacd91da6d
Nuke111 <45309966+Nuke111@users.noreply.github.com>
2021-06-16 19:31:34 +0900

    Revert "Revert "Revert "."""
    This reverts commit 99c11a4f54de9916bf2c8f56f425723ec0257f81.

--- algorithms/src/main/java/test/test.java
+++ algorithms/src/main/java/test/test.java
@@ -7,13 +7,37 @@ class test{
 
 	public static void main(String []argh)
 	{
-		String a = "23";
-		String b = "1234".substring(1,3);
-		Object c = b;
-		System.out.println(a.equals(b));
-		System.out.println(a.equals(c));
-		System.out.println(a==b);
-		System.out.println(a==c);
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNext()) {
+			Stack<Character> s = new Stack<>();
+			String input=sc.next();
+			for(int i=0; i<input.length(); i++) {
+				if(input.charAt(i)=='(' || input.charAt(i)=='[' || input.charAt(i)=='{'){
+					s.push(input.charAt(i));
+				} else {
+					switch (s.peek()) {
+					case '(':
+						if(input.charAt(i)==')')
+							s.pop();
+						break;
+					case '[':
+						if(input.charAt(i)==']')
+							s.pop();
+						break;
+					case '{':
+						if(input.charAt(i)=='}')
+							s.pop();
+						break;
+					}
+				}
+			}
+			if(!s.isEmpty())
+				System.out.println("false");
+			else
+				System.out.println("true");
+			s.clear();
+		}
 
 	}
 }
