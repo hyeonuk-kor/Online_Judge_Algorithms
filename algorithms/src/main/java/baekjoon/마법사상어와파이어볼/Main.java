7dee9fec14fd4d4067903c4929f84a2f5f8c144f
hyeonuk-kor <mrseos@naver.com>
2021-09-21 13:43:43 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/마법사상어와파이어볼/Main.java
+++ algorithms/src/main/java/baekjoon/마법사상어와파이어볼/Main.java
@@ -2,19 +2,27 @@ package baekjoon.마법사상어와파이어볼;
 import java.io.*;
 import java.util.*;
 public class Main {
-	static final int LIST = 0, MASS = 1;
+	static class Fireball {
+		int m, s, d;
+		public Fireball(int m, int s, int d) {
+			this.m = m;
+			this.s = s;
+			this.d = d;
+		}
+	}
 	static int N, M, K;
 	static int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 	static int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
-	static int board[][][];
-	static List<Queue<Integer>> speed, direction;
+	static List<ArrayList<Fireball>> list;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		initialize();
+		list = new ArrayList<>();
+		for(int i=0; i<N*N; i++)
+			list.add(new ArrayList<>());
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine()," ");
 			while(st.hasMoreTokens()) {
@@ -23,105 +31,63 @@ public class Main {
 				int m = Integer.parseInt(st.nextToken());
 				int s = Integer.parseInt(st.nextToken());
 				int d = Integer.parseInt(st.nextToken());
-				int index = board[LIST][y][x];
-				board[MASS][y][x] = m;
-				speed.get(index).add(s);
-				direction.get(index).add(d);
+				int index = y*N+x%N;
+				list.get(index).add(new Fireball(m,s,d));
 			}
 		}
-		for(int i=0; i<K; i++)
-			simulation();
+		System.out.println(simulation());
 	}
-	static void simulation() {
-		fireballMovement();
-		fireballWorking();
-		print();
+	static int simulation() {
+		for(int i=0; i<K; i++) {
+			movement();
+			mergeQuarter();
+		}
+		int sum = 0;
+		for(int i=0; i<list.size(); i++)
+			for(Fireball f: list.get(i)) 
+				sum += f.m;
+		return sum;
 	}
-	private static void print() {
-		for(int i=0; i<N; i++)
-			System.out.println(Arrays.toString(board[MASS][i]));
+	static void movement() {
+		List<ArrayList<Fireball>> next_list = new ArrayList<>();
+		for(int i=0; i<N*N; i++)
+			next_list.add(new ArrayList<>());
+		for(int i=0; i<list.size(); i++) {
+			for(Fireball f: list.get(i)) {
+				int ny = i/N, nx = i%N;
+				ny = range(ny+dy[f.d]*f.s%N);
+				nx = range(nx+dx[f.d]*f.s%N);
+				int next_index = ny*N + nx%N;
+				next_list.get(next_index).add(new Fireball(f.m, f.s, f.d));
+			}
+		}
+		list = next_list;
 	}
-	static void fireballMovement() {
-		Queue<int[]> q = new ArrayDeque<>();
-		for(int y=0; y<N; y++)
-			for(int x=0; x<N; x++)
-				if(board[MASS][y][x]!=0)
-					q.add(new int[] {y, x});
-		while(!q.isEmpty()) {
-			int[] point = q.poll();
-			int y = point[0];
-			int x = point[1];
-			if(board[MASS][y][x]!=0) {
-				int index = board[LIST][y][x];
-				while(!speed.isEmpty() && !direction.isEmpty()) {
-					int s = speed.get(index).poll();
-					int d = direction.get(index).poll();
-					int ny = y, nx = x;
-					for(int i=0; i<s; i++) {
-						ny = range(ny+dy[d]);
-						nx = range(nx+dx[d]);
-					}
-					board[MASS][ny][nx] += board[MASS][y][x];
-					board[MASS][y][x] = 0;
-					int next_index = board[LIST][ny][nx];
-					speed.get(next_index).add(s);
-					direction.get(next_index).add(d);
+	static void mergeQuarter() {
+		for(int i=0; i<list.size(); i++) {
+			List<Fireball> fireball_list = list.get(i);
+			if(fireball_list.size()>=2) {
+				int m=0, s=0, dir = 0;
+				for(Fireball f: fireball_list) {
+					m += f.m;
+					s += f.s;
+					dir += (f.d%2);
+				}
+				m /= 5;
+				s /= fireball_list.size();
+				dir = (dir>0 && dir<fireball_list.size())?1:0;
+				list.get(i).clear();
+				if(m==0) continue;
+				for(int q=0; q<4; q++) {
+					fireball_list.add(new Fireball(m, s, dir));
+					dir = dir +2;
 				}
 			}
 		}
 	}
-	static void fireballWorking() {
-		Queue<int[]> q = new ArrayDeque<>();
-		for(int y=0; y<N; y++)
-			for(int x=0; x<N; x++)
-				if(board[MASS][y][x]!=0)
-					q.add(new int[] {y, x});
-		while(!q.isEmpty()) {
-			int[] point = q.poll();
-			int y = point[0];
-			int x = point[1];
-			if(board[MASS][y][x]!=0) {
-				int index = board[LIST][y][x];
-				int m = board[MASS][y][x]/5;
-				int sum = 0;
-				for(int i: speed.get(index))
-					sum += i;
-				int s = sum/speed.get(index).size();
-				int dir = getDir(direction.get(index));
-			}
-		}
-	}
-	private static int getDir(Queue<Integer> q) {
-		int[] dir = new int[q.size()];
-		int index = 0;
-		while(!q.isEmpty()) {
-			dir[index++] = q.poll()%2;
-		}
-		int sum = 0;
-		for(int i=0; i<dir.length; i++)
-			sum += dir[i];
-		if(sum==0 || sum==q.size())
-			return 0;
-		return 1;
-	}
 	static int range(int point) {
-		if(point==-1)
-			return N-1;
-		else if(point==N)
-			return 0;
+		if(point<0) return point+N;
+		if(point>=N) return point-N;
 		return point;
-	}
-	static void initialize() {
-		board = new int[2][N][N];
-		speed = new ArrayList<>();
-		direction = new ArrayList<>();
-		int index = 0;
-		for(int i=0; i<N; i++) 
-			for(int j=0; j<N; j++)
-				board[LIST][i][j] = index++;
-		for(int i=0; i<index; i++) {
-			speed.add(new ArrayDeque<>());
-			direction.add(new ArrayDeque<>());
-		}
 	}
 }
