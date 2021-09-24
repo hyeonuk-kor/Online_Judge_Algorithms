c50d66d8b1705a78066cbd1c9deae7532af27753
hyeonuk-kor <mrseos@naver.com>
2021-09-24 16:08:30 +0900

    fastcampus 자료
    fastcampus 자료

---
+++ fastcampus/[강의자료]-part-2.-알고리즘-이론.zip--zip-압축파일-/Projects/CH30_PRIM_BASIC/src/Edge.java
@@ -0,0 +1,22 @@
+class Edge implements Comparable<Edge> {
+    public int weight;
+    public String node1;
+    public String node2;
+
+    public Edge(int weight, String node1, String node2) {
+        this.weight = weight;
+        this.node1 = node1;
+        this.node2 = node2;
+    }
+
+    // System.out.println() 으로 객체 자체 출력시, 다음 메서드를 호출하여, 원하는 attribute등을 출력할 수 있음
+    public String toString() {
+        return "(" + this.weight + ", " + this.node1 + ", " + this.node2 + ")";
+    }
+
+    @Override
+    public int compareTo(Edge edge)
+    {
+        return this.weight - edge.weight;
+    }
+}
---
+++ fastcampus/[강의자료]-part-2.-알고리즘-이론.zip--zip-압축파일-/Projects/CH30_PRIM_IMPROVED/src/Edge.java
@@ -0,0 +1,20 @@
+class Edge implements Comparable<Edge> {
+    public String node;
+    public int weight;
+
+    public Edge(String node, int weight) {
+        this.weight = weight;
+        this.node = node;
+    }
+
+    // System.out.println() 으로 객체 자체 출력시, 다음 메서드를 호출하여, 원하는 attribute등을 출력할 수 있음
+    public String toString() {
+        return "(" + this.weight + ", " + this.node + ")";
+    }
+
+    @Override
+    public int compareTo(Edge edge)
+    {
+        return this.weight - edge.weight;
+    }
+}
