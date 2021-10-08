8399128fd837f949de0c9fc8b14876b579b391c7
hyeonuk-kor <mrseos@naver.com>
2021-10-09 00:31:03 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/벽부수고이동하기/Main.java
+++ algorithms/src/main/java/baekjoon/벽부수고이동하기/Main.java
@@ -2,48 +2,75 @@ package baekjoon.벽부수고이동하기;
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int N, M, map[][];
-	static boolean visit[][][];
-	static int dy[] = {0, 0, 1, -1};
-	static int dx[] = {1, -1, 0, 0};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		visit = new boolean[N][M][2];
-		for(int i=0; i<N; i++) {
-			String line = br.readLine();
-			for(int j=0; j<M; j++) {
-				map[i][j] = line.charAt(j)-'0';
-			}
+	static class Solution {
+		BufferedReader br;
+		StringTokenizer st;
+		int N, M, board[][], visit[][];
+		int dir[][] = {{0,1},{0,-1},{1,0},{-1,0}};
+		int solve() {
+			input();
+			return bfs();
 		}
-		System.out.println(bfs());
-	}
-	static int bfs() {
-		Queue<int[]> q = new ArrayDeque<>();
-		visit[0][0][0] = true;
-		visit[0][0][1] = true;
-		q.add(new int[] {0, 0, 1});
-		while(!q.isEmpty()) {
-			int[] info = q.poll();
-			int y = info[0];
-			int x = info[1];
-			int move = info[2];
-			for(int d=0; d<4; d++) {
-				int ny = y + dy[d];
-				int nx = x + dx[d];
-				if(isRange(ny, nx)) {
-					
+		int bfs() {
+			Queue<Integer> q = new ArrayDeque<>();
+			visit[0][0] = 0;
+			q.add(0);
+			q.add(0);
+			q.add(0);
+			q.add(1);
+			while(!q.isEmpty()) {
+				int y = q.poll();
+				int x = q.poll();
+				int broken = q.poll();
+				int count = q.poll();
+				if(y==N-1 && x==M-1)
+					return count;
+				for (int d = 0; d < dir.length; d++) {
+					int ny = y + dir[d][0];
+					int nx = x + dir[d][1];
+					if(ny<0 || nx<0 || ny>=N || nx>=M)
+						continue;
+					if(visit[ny][nx]<=broken) continue;
+					if(board[ny][nx]==0) {
+						visit[ny][nx] = broken;
+						q.add(ny);
+						q.add(nx);
+						q.add(broken);
+						q.add(count+1);
+					} else {
+						if(broken==0) {
+							visit[ny][nx] = broken+1;
+							q.add(ny);
+							q.add(nx);
+							q.add(broken+1);
+							q.add(count+1);
+						}
+					}
 				}
 			}
+			return -1;
 		}
-		return -1;
+		void input() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				st = new StringTokenizer(br.readLine());
+				N = Integer.parseInt(st.nextToken());
+				M = Integer.parseInt(st.nextToken());
+				board = new int[N][M];
+				visit = new int[N][M];
+				for (int i = 0; i < N; i++) {
+					String input = br.readLine();
+					for (int j = 0; j < input.length(); j++) {
+						board[i][j] = input.charAt(j)-'0';
+						visit[i][j] = Integer.MAX_VALUE;
+					}
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
 	}
-	static boolean isRange(int y, int x) {
-		if(y<0 || x<0 || y>=N || x>=M)
-			return false;
-		return true;
+	public static void main(String[] args) {
+		System.out.println(new Solution().solve());
 	}
 }
