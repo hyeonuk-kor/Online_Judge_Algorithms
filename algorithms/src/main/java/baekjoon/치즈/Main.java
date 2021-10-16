daa1a90b94a4ec3151f9249e53fb28c58cb0bcb4
hyeonuk-kor <mrseos@naver.com>
2021-10-16 23:22:09 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/치즈/Main.java
+++ algorithms/src/main/java/baekjoon/치즈/Main.java
@@ -2,6 +2,76 @@ package baekjoon.치즈;
 import java.util.*;
 import java.io.*;
 public class Main {
+	static class P2638 {
+		BufferedReader br;
+		StringTokenizer st;
+		int N, M, board[][];
+		int cheese, time;
+		int dy[] = {0, 0, 1, -1};
+		int dx[] = {1,-1, 0,  0};
+		int visit[][];
+		void solve() {
+			input();
+			melting_cheese();
+		}
+		void melting_cheese() {
+			Queue<int[]> q = new ArrayDeque<>();
+			Queue<int[]> next_q = new ArrayDeque<>();
+			visit = new int[N][M];
+			q.add(new int[] {0, 0});
+			visit[0][0] = 1;
+			while(cheese>0) {
+				while(!q.isEmpty()) {
+					int[] info = q.poll();
+					int y = info[0];
+					int x = info[1];
+					for(int d=0; d<4; d++) {
+						int ny = y + dy[d];
+						int nx = x + dx[d];
+						if(nx<0 || ny<0 || ny>=N || nx>=M)
+							continue;
+						if(board[ny][nx]==0 && visit[ny][nx]==1)
+							continue;
+						if(visit[ny][nx]>=2)
+							continue;
+						if(board[ny][nx]==1 && visit[ny][nx]==1) {
+							board[ny][nx] = 0;
+							next_q.add(new int[] {ny, nx});
+						} else if(board[ny][nx]==0) {
+							q.add(new int[] {ny, nx});
+						}
+						visit[ny][nx]++;
+					}
+				}
+				q = new ArrayDeque<>(next_q);
+				time++;
+				cheese -= next_q.size();
+				next_q.clear();
+			}
+			System.out.println(time);
+		}
+		void input() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				st = new StringTokenizer(br.readLine());
+				N = Integer.parseInt(st.nextToken());
+				M = Integer.parseInt(st.nextToken());
+				board = new int[N][M];
+				visit = new int[N][M];
+				for(int i=0; i<N; i++) {
+					st = new StringTokenizer(br.readLine());
+					for(int j=0; j<M; j++) {
+						board[i][j] = Integer.parseInt(st.nextToken());
+						if(board[i][j]==1)
+							cheese++;
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	static class P2636 {
 		BufferedReader br;
 		StringTokenizer st;
@@ -70,7 +140,8 @@ public class Main {
 		}
 	}
 	public static void main(String[] args) {
-		P2636 t = new P2636();
+		//P2636 t = new P2636();
+		P2638 t = new P2638();
 		t.solve();
 	}
 }
