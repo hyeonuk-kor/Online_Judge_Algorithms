a1ae83ca8642e0b57ce24a04231ebfda36788ce7
hyeonuk-kor <mrseos@naver.com>
2021-10-20 20:50:37 +0900

    Update Main.java

--- algorithms/src/main/java/baekjoon/P1212_8진수2진수/Main.java
+++ algorithms/src/main/java/baekjoon/P1212_8진수2진수/Main.java
@@ -1,9 +1,10 @@
 package baekjoon.P1212_8진수2진수;
-import java.io.*;
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a = Integer.parseInt(br.readLine(), 8);
-		System.out.println(Integer.toBinaryString(a));
+import java.util.*;
+import java.math.BigInteger;
+class Main {
+	public static void main(String[] v) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = new BigInteger(sc.next(), 8);
+		System.out.println(a.toString(2));
 	}
 }
