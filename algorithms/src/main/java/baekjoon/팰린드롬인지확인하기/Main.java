a3935629dd0f6b1ebbb6c2d3616af3a1ad2c044b
hyeonuk-kor <mrseos@naver.com>
2021-10-28 00:41:21 +0900

    Create Main.java

---
+++ algorithms/src/main/java/baekjoon/팰린드롬인지확인하기/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.팰린드롬인지확인하기;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder input = new StringBuilder(br.readLine());
+		String A = input.toString();
+		String B = input.reverse().toString();
+		System.out.println(A);
+		System.out.println(B);
+		if(A.equals(B))
+			System.out.println(1);
+		else
+			System.out.println(0);
+	}
+}
