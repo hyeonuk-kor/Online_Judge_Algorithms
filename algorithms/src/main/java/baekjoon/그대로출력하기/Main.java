14c2112ee9dc9de2875a46d9dee12cb90613bf55
hyeonuk-kor <mrseos@naver.com>
2021-11-17 11:42:33 +0900

    Create Main.java

---
+++ algorithms/src/main/java/baekjoon/그대로출력하기/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.그대로출력하기;
+import java.io.*;
+public class Main {
+	static class P11718 {
+		BufferedReader br;
+		StringBuilder sb;
+		P11718() {
+			sb = new StringBuilder();
+			input();
+			System.out.println(sb);
+		}
+		void input() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				String input = br.readLine();
+				while(input!=null && !input.isEmpty()) {
+					sb.append(input);
+					input = br.readLine();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		new P11718();
+	}
+}
