c8b9a4036c63077198848e714b9fb1d0b193904c
hyeonuk-kor <mrseos@naver.com>
2021-11-30 02:35:50 +0900

    Create Main.java

---
+++ algorithms/src/main/java/baekjoon/어항정리/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.어항정리;
+import java.io.*;
+import java.util.*;
+public class Main {
+	static class P23291 {
+		
+	}
+	public static void main(String[] args) {
+		new P23291();
+	}
+}
